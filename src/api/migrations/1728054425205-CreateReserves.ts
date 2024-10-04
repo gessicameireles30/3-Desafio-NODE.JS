d089f7c7fe74595090d5f66e10ed443ed4bc1742
gessicameireles30 <gessicadasilvameireles@gmail.com>
2024-10-04 12:09:37 -0300

    build: add migrations

---
+++ src/api/migrations/1728054425205-CreateReserves.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateReserves1728054425205 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
